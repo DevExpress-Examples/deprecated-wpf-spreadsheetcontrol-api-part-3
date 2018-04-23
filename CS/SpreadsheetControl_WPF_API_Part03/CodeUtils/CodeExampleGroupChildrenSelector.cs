3e64b83f5c7ae9206c4d843792dbd4b0d0ba290b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SpreadsheetControl_WPF_API_Part03/CodeUtils/CodeExampleGroupChildrenSelector.cs
@@ -0,0 +1,20 @@
+﻿using DevExpress.Xpf.Grid;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpreadsheetControl_WPF_API_Part03
+{
+    public class CodeExampleGroupChildrenSelector : IChildNodesSelector
+    {
+        IEnumerable IChildNodesSelector.SelectChildren(object item)
+        {
+            if (item is CodeExampleGroup)
+                return ((CodeExampleGroup)item).Examples;
+            return null;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,10 @@
+# WPF SpreadsheetControl API - Part 3
+
+
+<p>This example is the third part of the SpreadsheetControl API set of examples that demonstrates how to use the SpreadsheetControl API to programmatically manage spreadsheet documents, without the need for Microsoft Excel to be installed.</p>
+<p>This sample introduces API properties and methods used to perform the following operations:</p>
+<p>- Apply data validation to control the type of data or the values that users enter into a cell</p>
+
+<br/>
+
+
